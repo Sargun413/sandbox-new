de03741944ae9eb717b34cc5401f88e343cbe679
Sargun413 <sargungujral13@gmail.com>
2022-09-13 13:45:10 -0700

    adding fib algorithm

---
+++ src/main/java/org/example/algorithm/Fibonacci.java
@@ -0,0 +1,30 @@
+package org.example.algorithm;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.sql.SQLOutput;
+
+public class Fibonacci {
+
+    public static int fib(int n) {
+        if (n == 0) {
+            return 0;
+        }
+
+        if (n == 1) {
+            return 1;
+        }
+
+        return fib(n - 1) + fib(n - 2);
+    }
+
+    public static void main(String[] args) {
+
+
+        System.out.println(fib(0));
+        System.out.println(fib(1));
+        System.out.println(fib(5));
+    }
+
+}
+
